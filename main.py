de7612e70664fa55718752a779d2c2e20e3323d8
Bublik78 <115350360+Bublik78@users.noreply.github.com>
2025-04-14 18:40:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import time
 print('Эта программа позволяет делать записи в дневнике.')
 print('Дневник - файл "swag.txt"')
 
-swag = open('swag.txt', 'a')
-swag.write(f'{time.asctime()} \n')
-print('Вводи сюда.')
-user_input = input('')
-swag.write(f'{user_input} \n\n')
+with open('swag.txt', 'a', encoding='utf-8') as swag:
+    swag.write(f'{time.asctime()} \n')
+    print('Вводи свой стафф. А чтобы выйти введи "выход"')
+    user_input = input('')
+
+    while user_input.lower() != 'выход':
+        swag.write(f'{user_input} \n')
+        user_input = input('')
+
+    swag.write('\n--- Конец записи ---\n')
